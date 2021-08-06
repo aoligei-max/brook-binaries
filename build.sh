35f69b1450e3e2b472200a12de7cd1899a1fc395
aoligei-max <62712238+aoligei-max@users.noreply.github.com>
2021-08-07 00:01:55 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@ release_name="brook"
 repo="txthinking/brook"
 
 get_releases(){
+    cd ${version}
     links=`curl https://api.github.com/${repo}/releases | grep https://github.com/${repo}/releases/download/${version} | awk '{print $2}'`
     echo links | while read link
     do
-        wget ${link} -O ${version}/
+        wget ${link} -O ${version}
     done
-    zip -9 ./${version}/BrookLite.exe.zip ./${version}/BrookLite.exe
+    zip -9 BrookLite.exe.zip rookLite.exe
+    cd ..
 }
 
 update(){
